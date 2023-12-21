5f99bc174252ee99c411eb18d3cb68436d3c63f1
Kooreis <kooreis.official@gmail.com>
2023-12-21 15:53:03 -0600

    In the second update, two Room objects are created: 'currentRoom' and 'northRoom'. The 'currentRoom' is set to have a door leading to the 'northRoom', and the 'northRoom' is set to have a door leading back to the 'currentRoom'.

--- Simple_Text_Adventure_Engine.java
+++ Simple_Text_Adventure_Engine.java
@@ -1,7 +1,4 @@
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
-}
+Room currentRoom = new Room("You are in a dark room. There is a door to the north.");
+Room northRoom = new Room("You are in a bright room. There is a door to the south.");
+currentRoom.setNorth(northRoom);
+northRoom.setSouth(currentRoom);
