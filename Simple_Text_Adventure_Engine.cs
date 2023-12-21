5892b13a7cbb4a9599e907a317156352a5bc5cde
Kooreis <kooreis.official@gmail.com>
2023-12-21 15:53:03 -0600

    This is the Game class. It contains the main game loop, which continuously prompts the user for input and handles it. It also initializes the player and the starting room.

--- Simple_Text_Adventure_Engine.cs
+++ Simple_Text_Adventure_Engine.cs
@@ -1,10 +1,18 @@
-using System;
-
-public class Program
+public class Game
 {
-    static void Main(string[] args)
+    private Room currentRoom;
+    private Player player;
+
+    public void Start()
     {
-        Game game = new Game();
-        game.Start();
+        player = new Player();
+        currentRoom = new Room("You are in a dark room. There is a door to the north.");
+
+        while (true)
+        {
+            Console.WriteLine(currentRoom.Description);
+            string input = Console.ReadLine();
+            HandleInput(input);
+        }
     }
 }
