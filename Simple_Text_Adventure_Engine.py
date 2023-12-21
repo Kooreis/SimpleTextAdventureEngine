6393fd25c02c2d389b949b1949e2eff28449a02b
Kooreis <kooreis.official@gmail.com>
2023-12-21 15:53:03 -0600

    Created a Player class with initialization method. The method initializes the current room of the player.

--- Simple_Text_Adventure_Engine.py
+++ Simple_Text_Adventure_Engine.py
@@ -1,9 +1,3 @@
-class Room:
-    def __init__(self, description, north, south, east, west, up, down):
-        self.description = description
-        self.north = north
-        self.south = south
-        self.east = east
-        self.west = west
-        self.up = up
-        self.down = down
+class Player:
+    def __init__(self, current_room):
+        self.current_room = current_room
